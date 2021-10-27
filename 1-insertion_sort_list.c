44e3ad535f502a67df41a4157fcd5ae7c194c267
DAN OMWA OPIYO <danomwaopiyo@gmail.com>
2021-10-27 04:33:35 +0300

    sorts a doubly linked list of integers in ascending order using insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,64 +1,54 @@
 #include "sort.h"
 
-
 /**
- * swapprev - swaps a node with the previous node in a doubly linked list
- * @current: the current node
- * @head: head of the doubly linked list
+ * swap - swaps 2 nodes in a doubly-linked list
+ * @a: address of first node
+ * @b: address of second node
+ *
+ * Return: void
  */
-void swapprev(listint_t *current, listint_t **head)
+void swap(listint_t *a, listint_t *b)
 {
-	listint_t *tmp = NULL;
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
 
-	tmp = current->prev;
-	if (current->next)
-		current->next->prev = tmp;
-	if (tmp->prev)
-		tmp->prev->next = current;
-	else
-		*head = current;
-
-	current->prev = current->prev->prev;
-	tmp->next = current->next;
-	tmp->prev = current;
-	current->next = tmp;
 }
+
 /**
- * insertion_sort_list - implemetnation of insertion sort in
- * a doubly linked list
- * @list: a pointer to the head of the list to be sorted
+ * insertion_sort_list - insertion sorts a doubly-linked list
+ * @list: address of pointer to head node
+ *
+ * Return: void
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = NULL, *prev_tmp = NULL, *prev = NULL;
+	listint_t *i, *j;
 
-	if (!list)
+	if (!list || !*list || !(*list)->next)
 		return;
-	current = *list;
-	while (current)
+	i = (*list)->next;
+	while (i)
 	{
-		prev = current->prev;
-		/*move the current element some steps back until in position*/
-		while (prev)
+		j = i;
+		i = i->next;
+		while (j && j->prev)
 		{
-			if (current->n >= prev->n)
-				break;
-			/*swap the two nodes if they are out of order*/
-			swapprev(current, list);
-
-			/*once the swap is done swap the names*/
-			prev_tmp = prev;
-			prev = current;
-			current = prev_tmp;
-			print_list(*list);
-			/*
-			 * go back one step so that the newly swaped(went to left) value
-			 * will be your current and make sure it isn't smaller than
-			 * the value in its left
-			 */
-			prev = prev->prev;
-			current = current->prev;
+			if (j->prev->n > j->n)
+			{
+				swap(j->prev, j);
+				if (!j->prev)
+					*list = j;
+				print_list((const listint_t *)*list);
+			}
+			else
+				j = j->prev;
 		}
-		current = current->next;
+
 	}
 }
